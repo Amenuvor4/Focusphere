56432848edacbe6dfbe6ec232b406894c9243085
Darius Amenuvor <daarius24@gmail.com>
2025-01-23 09:36:02 -0600

    login page test

---
+++ frontend/my-app/src/pages/Login.js
@@ -0,0 +1,13 @@
+const Login = () => {
+    return (
+        <div className="text-center mt-10">
+            <h1 className="text-3x1 font-bold">Test Login Page</h1>
+            <p className="mt-2 text-gray-600"> This is where users login.</p>
+        </div>
+
+    );
+
+};
+
+
+export default Login;
